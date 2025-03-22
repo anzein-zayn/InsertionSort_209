f94085e3841b1ffb7cda67ae9861c170e5184606
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-03-23 02:18:37 +0700

    Menampilkan Output ke Layar

--- ContohInsertionSort.cpp
+++ ContohInsertionSort.cpp
@@ -54,5 +54,16 @@ void InsertionSort()
 
 void display()
 {
-    
+    cout << endl;
+    cout << "========================" << endl;
+    cout << "Total Pass = " << n - 1 << endl;
+    cout << "========================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "========================" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
 }
